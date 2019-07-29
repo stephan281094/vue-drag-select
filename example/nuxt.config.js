d0e71b60b3a5c204baf9d6ced34dde4125d46fbf
Stephan de Vries <stephan281094@gmail.com>
2019-07-29 14:09:50 +0200

    Update example to the latest version of Nuxt

---
+++ example/nuxt.config.js
@@ -0,0 +1,15 @@
+export default {
+  mode: "spa",
+  head: {
+    title: process.env.npm_package_name || "",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
+    ]
+  }
+};
--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,91 +1,88 @@
 <template>
   <div class="app">
     <h1>Vue Drag Select Example</h1>
-    <no-ssr>
-      <drag-select-container selectorClass="item">
-        <template slot-scope="{ selectedItems }">
-          <div
-            v-for="item in 50"
-            :class="getClasses(item, selectedItems)"
-            :data-item="item"
-          >
-            Item {{ item }}
-          </div>
-        </template>
-      </drag-select-container>
-    </no-ssr>
+    <drag-select-container selectorClass="item">
+      <template slot-scope="{ selectedItems }">
+        <div
+          v-for="item in 50"
+          :key="item"
+          :class="getClasses(item, selectedItems)"
+          :data-item="item"
+        >
+          Item {{ item }}
+        </div>
+      </template>
+    </drag-select-container>
   </div>
 </template>
 
 <script>
-  import DragSelect from 'vue-drag-select'
-  import NoSSR from 'vue-no-ssr'
+import DragSelect from "vue-drag-select";
 
-  export default {
-    name: 'home',
+export default {
+  name: "home",
 
-    components: {
-      'drag-select-container': DragSelect,
-      'no-ssr': NoSSR
-    },
+  components: {
+    "drag-select-container": DragSelect
+  },
 
-    methods: {
-      getClasses (item, selectedItems) {
-        const isActive = !!(selectedItems.find((selectedItem) => {
-          return parseInt(selectedItem.dataset.item, 10) === item
-        }))
+  methods: {
+    getClasses(item, selectedItems) {
+      const isActive = !!selectedItems.find(selectedItem => {
+        return parseInt(selectedItem.dataset.item, 10) === item;
+      });
 
-        return {
-          item: true,
-          active: isActive
-        }
-      }
+      return {
+        item: true,
+        active: isActive
+      };
     }
   }
+};
 </script>
 
 <style>
-  /* Basic reset */
-  *,
-  *:before,
-  *:after {
-    box-sizing: inherit;
-  }
+/* Basic reset */
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
 
-  html {
-    box-sizing: border-box;
-    user-select: none;
-  }
+html {
+  box-sizing: border-box;
+  user-select: none;
+}
 
-  html,
-  body {
-    margin: 0;
-    padding: 0;
-    min-height: 100vh;
-  }
+html,
+body {
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
+}
 
-  body {
-    font: 16px / 1.5 'Helvetica Neue', sans-serif;
-    padding: 5%;
-  }
+body {
+  font: 16px / 1.5 "Helvetica Neue", sans-serif;
+  padding: 5%;
+}
 
-  /* Custom styling */
-  .item {
-    display: inline-flex;
-    min-width: 80px;
-    height: 100px;
-    margin-right: 10px;
-    margin-bottom: 10px;
-    background-color: #ddd;
-    justify-content: center;
-    align-items: center;
-    text-transform: uppercase;
-    letter-spacing: 3px;
-    font-size: 10px;
-  }
+/* Custom styling */
+.item {
+  display: inline-flex;
+  min-width: 80px;
+  height: 100px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  background-color: #ddd;
+  justify-content: center;
+  align-items: center;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  font-size: 10px;
+}
 
-  .item.active {
-    background-color: rgb(0, 162, 255);
-    color: #fff;
-  }
+.item.active {
+  background-color: rgb(0, 162, 255);
+  color: #fff;
+}
 </style>
